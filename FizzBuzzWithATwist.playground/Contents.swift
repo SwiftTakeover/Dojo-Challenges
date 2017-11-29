e0433cfbb00491465cbc85a10f93bcdb16f7d6d1
Keegan Rush <thecodedself@gmail.com>
2017-11-29 05:00:32 +0100

    Change one of the challenges to use a Range rather than variadic parameters

--- FizzBuzzWithATwist.playground/Contents.swift
+++ FizzBuzzWithATwist.playground/Contents.swift
@@ -7,12 +7,12 @@ func fizzBuzz(_ numbers: [Int]) -> [String] {
 }
 
 /*:
- ## That takes an array of consecutive integers:
+ ## That takes an array of consecutive integers
  `fizzBuzz([1, 2, 3, 4, 5])`
  */
 
 /*:
- ## Return an array of the same length consisting of the following strings:
+ ## Return an array of the same length consisting of the following strings
  - 'Fizz' if the number at the same index is divisible by 3
  - 'Buzz' if the number at the same index is divisible by 5
  - 'FizzBuzz' if the number at the same index is divisible by 3 and 5
@@ -30,7 +30,7 @@ func fizzBuzz(_ numbers: [Int]) -> [String] {
  - TDD your solution
  - 'Fizz' if the number at the same index is divisible by 3, or has a 3 in it
  - 'Buzz' if the number at the same index is divisible by 5, or has a 5 in it
- - Convert the array to a variadic parameter
+ - Convert the array to a Range parameter
  - Rather than converting the number directly to a string if it is neither Fizz nor Buzz (e.g. '7' or '8'), return the text representation of the number (e.g. 'Seven' or 'Eight')
  */
 
